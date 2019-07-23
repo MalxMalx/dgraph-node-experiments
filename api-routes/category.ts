c26c76580c294c750771917738cc0b24525eb19d
Alexander Malka <alexandrmalka@gmail.com>
2019-07-23 16:54:34 +0300

    Removed commented code

--- api-routes/category.ts
+++ api-routes/category.ts
@@ -1,33 +1,7 @@
 import * as Router from 'koa-router';
 import { getCategory } from '../handlers/category-operations';
 
-// const dgraphOperationManager = require('../operation-manager');
-
 const router = new Router();
-
-// router.post('/category', async ctx => {
-//   const {
-//     name,
-//     description,
-//     tagIds // array of strings
-//   } = ctx.request.body;
-
-//   try {
-//     const result = await dgraphOperationManager.setCategories({
-//       name,
-//       description,
-//       tagIds
-//     });
-
-//     ctx.body = result;
-//   } catch (error) {
-//     if (error.message === 'already exists') {
-//       return ctx.throw(400);
-//     }
-
-//     ctx.throw(500, error);
-//   }
-// });
 
 router.get('/category/:id', async ctx => {
   const { id } = ctx.params;
