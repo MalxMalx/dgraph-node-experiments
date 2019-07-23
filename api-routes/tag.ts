4a1b7acdcfebe9dafb52b4aaa50a08fdc0d6ab15
Alexander Malka <alexandrmalka@gmail.com>
2019-07-23 16:55:13 +0300

    Removed commented code

--- api-routes/tag.ts
+++ api-routes/tag.ts
@@ -1,32 +1,7 @@
 import * as Router from 'koa-router';
 import { getTagById } from '../handlers/tag-operations';
 
-// const dgraphOperationManager = require('../operation-manager');
 const router = new Router();
-
-// router.post('/tag', async ctx => {
-//   const {
-//     name,
-//     description,
-//     categoryIds // array of strings
-//   } = ctx.request.body;
-
-//   try {
-//     const result = await dgraphOperationManager.setTag({
-//       name,
-//       description,
-//       categoryIds
-//     });
-
-//     ctx.body = result;
-//   } catch (error) {
-//     if (error.message === 'already exists') {
-//       return ctx.throw(400);
-//     }
-
-//     ctx.throw(500, error);
-//   }
-// });
 
 router.get('/tag/:id', async ctx => {
   const { id } = ctx.params;
@@ -39,35 +14,5 @@ router.get('/tag/:id', async ctx => {
 
   ctx.body = getResult;
 });
-
-// router.patch('/tag/:id', async ctx => {
-//   const { id } = ctx.params;
-//   const tagData = ctx.request.body;
-
-//   try {
-//     ctx.body = await dgraphOperationManager.updateTag(id, tagData);
-//   } catch (error) {
-//     console.log(error);
-//     if (error.message === 'Tag was not found') {
-//       return ctx.throw(404);
-//     }
-//     ctx.throw(500);
-//   }
-// });
-
-// router.delete('/tag/:id', async ctx => {
-//   const { id } = ctx.params;
-
-//   try {
-//     await dgraphOperationManager.deleteTag(id);
-//     ctx.status = 204;
-//   } catch (error) {
-//     console.log(error);
-//     if (error.message === 'Tag was not found') {
-//       return ctx.throw(404);
-//     }
-//     ctx.throw(500);
-//   }
-// });
 
 export default router;
